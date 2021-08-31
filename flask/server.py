85470e21ca7cab75ac04b9e185a7067d1f959a15
Marina Krivtsoun <marinaktsn@gmail.com>
2021-08-31 22:35:47 +0300

    tried docker-compose, realised it is overkill for the task

--- flask/server.py
+++ flask/server.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import os
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient("mongo:27017")
 
 @app.route('/')
 def index():
-    return "Shop home page"
+    try:
+        client.admin.command('ismaster')
+    except:
+        return "Server not available"
+    return "Shop home page, the MongoDB client is running!\n"
 
 @app.route('/phones/', methods=['POST'])
 def create_phone():
@@ -23,4 +30,4 @@ def delete_phone():
     return "Under development"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
