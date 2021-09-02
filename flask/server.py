622e3136f9ab18e571aae2a8e8926ca9ea1dab77
Marina Krivtsoun <marinaktsn@gmail.com>
2021-09-02 09:11:42 +0300

    dockerised and changed readme

--- flask/server.py
+++ flask/server.py
@@ -1,23 +1,25 @@
-from flask import Flask, jsonify, request
-# from flask import Flask, json, jsonify, request
+#!/usr/bin/env python
 import os
+
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
+
 client = MongoClient("mongo:27017")
 # how about initialising it with a test item and a decorator that only allows a func run once?
 db = client['products']
 collection = db['phones']
 
 @app.route('/')
-def index():
+def todo():
     try:
         client.admin.command('ismaster')
     except:
         return jsonify({'message': 'Server not available'})
     return jsonify({'status': '200', 'message': 'Shop home page, the MongoDB client is running!\n'})
 
-@app.route('/api/phones/', methods=['POST'])
+@app.route('/api/phone/', methods=['POST'])
 def create_phone():
     try:
         # look up if I need to check for sql injections in NoSql
@@ -30,9 +32,6 @@ def create_phone():
             'description': data['description'],
             'parameters': data['parameters']
         }
-        #     data_created = collection.insert_many(data)
-        #     return data_created.inserted_ids, 201
-        # else:
         data_created = collection.insert_one(prod).inserted_id
         return jsonify({'id assigned to new phone product': data_created}), 201
     except:
@@ -42,6 +41,7 @@ def create_phone():
 def read_phone(id):
     try:
         data_fetched = collection.find_one({"_id": id})
+        # check if jsonify or json.dumps work fine
         return jsonify(data_fetched)
     except:
         return jsonify({'message': 'No product found'}), 404
@@ -64,31 +64,7 @@ def read_phones():
                 return jsonify({'message': 'No products in db'}), 404
     except:
         return jsonify({'message': 'request unavailable'}), 400
-    # try:
-        # result = {}
-        # productlist = []
-        # if data:
-            # for product in collection.find(data):
-            #     prod = {
-            #         'name': product['name'],
-            #         'description': product['description'],
-            #         'parameters': product['parameters']
-            #     }
-            #     productlist.append(prod)
-        # else:
-        #     data_fetched = collection.find()
-        # data_fetched = collection.find({"_id": id})
-    #     return json.dumps(data_fetched)
-    # except:
-    #     return jsonify({'message': 'No product found'}), 404
-
-# @app.route('/api/phones/', methods=['PUT'])
-# def update_phone():
-#     return "Under development"
-
-# @app.route('/api/phones/', methods=['DELETE'])
-# def delete_phone():
-#     return "Under development"
-
+   
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
+
