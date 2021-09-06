e26365b223450b8864086df624d9e819cc676cb0
Marina Krivtsoun <marinaktsn@gmail.com>
2021-09-06 08:07:04 +0300

    create db options tested

--- README.md
+++ README.md
@@ -4,13 +4,11 @@ The project features a backend for a shop using MongoDB on localhost:27017, Flas
 Installation:
 
 - install docker and docker-compose:
-'''
 https://www.docker.com/
-'''
 - clone project:
-"""
+```
 git clone https://github.com/Meao/FlaskMongo.git
-"""
+```
 - build and run:
 ```
 docker-compose up -d
@@ -19,8 +17,16 @@ docker-compose up -d
 API usage:
 
 - create:
+```
 curl -X POST "http://127.0.0.1:80/api/phone/" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"name\":\"Redmi\",\"description\":\"mobile phone\",\"parameters\":[{\"OS\":\"Android\"},{\"color\":\"green\"}]}"
+curl -X POST "http://127.0.0.1:80/api/phone/" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"name\":\"iPhone\",\"description\":\"mobile phone\",\"parameters\":[{\"OS\":\"iOS\"},{\"color\":\"white\"}]}"
+curl -X POST "http://127.0.0.1:80/api/phone/" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"name\":\"iPhone\",\"description\":\"mobile phone\",\"parameters\":[{\"OS\":\"iOS\"},{\"model\":\"5S\"}]}"
+```
 - read one:
+```
 curl -X GET "http://127.0.0.1:80/api/phone/5fbc06cb11bcf5cbbd8b6c3d" -H  "accept: application/json"
+```
 - read filtering by parameters:
+```
 curl -X GET "http://127.0.0.1:80/api/phones/?OS=Android&color=green" -H  "accept: application/json"
+```
--- flask/server.py
+++ flask/server.py
@@ -5,11 +5,12 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
-
+# client = MongoClient(os.environ['PRODUCTS_DB_1_PORT_27017_TCP_ADDR'],27017)
 client = MongoClient("mongo:27017")
-# how about initialising it with a test item and a decorator that only allows a func run once?
-db = client['products']
-collection = db['phones']
+# # how about initialising it with a test item and a decorator that only allows a func run once?
+# db = client['products']
+# collection = db['phones']
+db = client.productsdb
 
 @app.route('/')
 def todo():
@@ -27,20 +28,23 @@ def create_phone():
     except:
         return jsonify({'message': 'request unavailable'}), 400
     try:
+        # need to create a dict structure for params
         prod = {
             'name': data['name'],
             'description': data['description'],
             'parameters': data['parameters']
         }
-        data_created = collection.insert_one(prod).inserted_id
-        return jsonify({'id assigned to new phone product': data_created}), 201
+        # data_created = collection.insert_one(prod).inserted_id
+        data_created = db.productsdb.insert_one(prod)
+        return jsonify({'id assigned to new phone product': data_created.inserted_id}), 201
     except:
         return jsonify({'message': 'db error'})
 
 @app.route('/api/phone/<id>', methods=['GET'])
 def read_phone(id):
     try:
-        data_fetched = collection.find_one({"_id": id})
+        # data_fetched = collection.find_one({"_id": id})
+        data_fetched = db.productsdb.find_one({"_id": id})
         # check if jsonify or json.dumps work fine
         return jsonify(data_fetched)
     except:
@@ -51,13 +55,15 @@ def read_phones():
     try:
         query_params = request.get_json(force=True)
         if query_params:
-            data_fetched = collection.find(query_params)
+            # data_fetched = collection.find(query_params)
+            data_fetched = db.productsdb.find(query_params)
             if data_fetched.count() > 0:
                 return jsonify(data_fetched)
             else:
                 return jsonify({'message': 'No products found'}), 404
         else:
-            data_fetched = collection.find()
+            # data_fetched = collection.find()
+            data_fetched = db.productsdb.find()
             if data_fetched.count() > 0:
                 return jsonify(data_fetched)
             else:
